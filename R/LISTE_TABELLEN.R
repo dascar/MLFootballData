eb138b43650c971a540cffebba5b644ec92baef5
dascar <andreas.wygrabek@da-sc-ar.de>
2019-08-13 06:03:01 +0200

    init

---
+++ R/LISTE_TABELLEN.R
@@ -0,0 +1,11 @@
+#' Tabelleninformationen aus Bundesliga und Premier League
+#'
+#' Die Datensätze LISTE_TABELLEN_BUNDESLIGA bzw. LISTE_TABELLEN_PREMIER_LEAGUE enthalten Tabelleninformationen. Jeder Datensatz
+#' bildet eine verschachtelte Liste. Die erste Ebene ist die Saison. Die zweite Ebene die Spieltage der jeweiligen Saison.
+#' @docType data
+#' @usage data(LISTE_TABELLEN_BUNDESLIGA); data(LISTE_TABELLEN_PREMIER_LEAGUE)
+#' @keywords datasets
+#' @examples
+#' data(LISTE_TABELLEN_BUNDESLIGA)
+#' data(LISTE_TABELLEN_PREMIER_LEAGUE)
+"LISTE_TABELLEN_BUNDESLIGA"
---
+++ R/MAINFACT_BUNDESLIGA.R
@@ -0,0 +1,28 @@
+#' Begegnungen Bundesliga
+#'
+#' Der Datensatz enthält alle Bundesligabegegnungen über einen Zeitraum von 11 Jahren - Saison 2005/2006 bis einschließlich Saison 2015/2016. Jeder Begegnung sind
+#' Informationen über das jeweilige Spiel, Tabelleninformationen sowie Wettquoten angefügt. Die Tabelleninformationen jeder Zeile verfügen über einen Lag -
+#' sie geben an, welche Tabellenposition die Mannschaften VOR der Begegnung innehaben.
+#'
+#' @docType data
+#' @usage data(MAINFACT_BUNDESLIGA)
+#'
+#'
+# #' @format A data frame with 48 rows and 4 variables:
+# #' \describe{
+# #'   \item{SAISON_ID}{Saison}
+# #'   \item{Datum}{Tag der Begegnung}
+# #'   \item{HeimTeam}{Heimmannschaft}
+# #'   \item{AuswTeam}{Auswärtsteam}
+# #' }
+#'
+#' @details
+#'
+#' Die Rohdaten für die Aufbereitung der beiden zentralen Datensätze MAINFACT_BUNDESLIGA und MAINFACT_PREMIER_LEAGUE im Paket entstammen der Webseite \href{http://www.football-data.co.uk/data.php}.
+#' Ein Mapping zwischen Spaltenbezeichnungen in den Rohdaten und den Daten dieses Paktes liegt in der Datei CODEBOOK_QUOTA_DATA.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(MAINFACT_BUNDESLIGA)
+"MAINFACT_BUNDESLIGA"
